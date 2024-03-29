5805b1b9d0818f6290e1ddbd418721497cb3c7f7
vaishnaviparopate <vaishnaviparopate20@gmail.com>
2024-03-28 21:46:12 -0700

    adding  assignment

---
+++ ass.js
@@ -0,0 +1,112 @@
+// 1. Take two numbers a,b and print the sum, difference, product, quotient and modulus of a & b
+
+// let a=5;
+// let b=10;
+// console.log(a+b); // sum
+// console.log(a-b); //differrence
+// console.log(a*b); //product
+// console.log(a/b); //quotient
+// console.log(a%b); //module
+
+
+// 2. Take two numbers and only print the integer part when a is divided by b
+
+// let c=10;
+// let d=3;
+// console.log(Math.floor(c/d))
+// let q=c/d
+// let r=c%d
+// console.log(r/d);
+// console.log(q-r/d);
+
+
+// 3. Take two numbers a,b and print the exponential power of the first number raised by the second.
+// let c=5;
+// let d=10;
+// console.log(c**d);
+
+// 4. Take two numbers a,b and only print the decimal part of the result obtained when a is divided by b.
+let c=10;
+let d=3;
+let r=c%d
+console.log(r/d);
+
+
+
+// 5. Take marks in 5 subjects A,B,C,D,E and print out the average of marks from five subject marks.
+// let A=90;
+// let B=95;
+// let C=98;
+// let D=96;
+// let E=97;
+// let average=A+B+C+D+E/5;
+// console.log(average)
+
+// 6. Take the cost price a and selling price b of a product, and print the profit obtained on the product.
+// let a=467;
+// let b=787;
+// console.log(a-b)
+
+// 7. Take a number and print out the last digit of the number.
+// let a=908;
+// console.log(a%10);
+
+// 8. Take a 2 digit number and return the first and last digits
+// let a=67;
+// console.log(Math.floor(a/10))
+// console.log(a%10); 
+
+
+// 9. Write a JavaScript program to find the largest of three numbers.
+// let a=45;
+// let b=67;
+// let c=90;
+// max=0
+// if (a>b && a>c){
+// max=a;
+// console.log(max);
+// }
+// else if(b>c && b>a){
+//     max=b;
+// console.log(max);
+
+// }
+// else{
+//     max=c;
+// console.log(max);
+
+// }
+
+// 10. Write a JavaScript program to check whether a number is even or odd.
+// let a=68;
+// if(a%2==0){
+//     console.log("a is even number")
+// }
+// else{
+//     console.log("a is not even number")
+// }
+
+// 11. Write a JavaScript program to check whether a given number is prime or not.
+// let a=67;
+// let c=0;
+// let i=0;
+// while (i<=a){
+//     if (a%i==0){
+//         c=c+1;
+//     }
+//     i=i+1;
+// }
+// if(c==2){
+//     console.log("it is a prime number");
+// }
+// else{
+//     console.log("it is not prime number")
+// }
+
+// 12. Write a JavaScript program to convert temperature from Celsius to Fahrenheit and vice versa.
+// let c=100;
+// let f=(c*9/5)+32;
+// console.log(f);
+ 
+
+
